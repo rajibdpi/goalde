25f5df865c1848b0d689635c900076b5be3ff861
Al Amin Islam <alaminbinmonsur@gmail.com>
2022-08-24 22:38:32 +0600

    responsive stylesheet added

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+// upcoming-match-slider
+$(document).ready(function () {
+    $('.upcoming-match-slider').owlCarousel({
+        loop: true,
+        infinite: true,
+        margin: 10,
+        responsiveClass: true,
+        items: 1,
+        nav: true,
+        dots: false,
+    });
+
+    var owl = $('.upcoming-match-slider');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.customNextBtn').click(function () {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    });
+});
+
+// Tranding slider
+$(document).ready(function () {
+    $('.tranding-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        items: 3,
+        nav: true,
+        dots: false,
+    });
+
+    var owl = $('.tranding-slider');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.trandingNextBtn').click(function () {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.trandingPrevBtn').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    });
+});
+
+// Footer Img slider
+$(document).ready(function () {
+    $('.footer-img-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        items: 5,
+        nav: true,
+        dots: false,
+    });
+
+    var owl = $('.footer-img-slider');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.footerSliderNextBtn').click(function () {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.footerSliderPrevBtn').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    });
+});
